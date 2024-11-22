ee6f99809e20fd97560fbd1e9c9e3adbe50006ff
xioyo <68426327+xioyo@users.noreply.github.com>
2024-11-22 18:31:55 +0530

    sunfish: Add Maintainer name

--- lineage_sunfish.mk
+++ lineage_sunfish.mk
@@ -19,6 +19,7 @@ TARGET_BOOT_ANIMATION_RES := 1080
 
 # Gapps
 WITH_GMS := true
+TARGET_CORE_GMS := true
 TARGET_DEFAULT_PIXEL_LAUNCHER := true
 
 # Extra Flags.
@@ -32,6 +33,8 @@ PRODUCT_MODEL := Pixel 4a
 PRODUCT_NAME := lineage_sunfish
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
+    RisingChipset="Snapdragon730G" \
+    RisingMaintainer="xioyo" \
     BuildDesc="sunfish-user 13 TQ3A.230805.001.S1 10786265 release-keys" \
     BuildFingerprint=google/sunfish/sunfish:13/TQ3A.230805.001.S1/10786265:user/release-keys \
     DeviceProduct=sunfish
